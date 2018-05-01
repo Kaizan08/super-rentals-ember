427cb0e79ca5433d9b925889740959f358e0b4a5
Kaizan08 <jlsnyder08@gmail.com>
2018-05-01 13:00:24 -0500

    finished simple component

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -1,9 +1,37 @@
 import { module, test } from 'qunit';
-import { click, visit, currentURL } from '@ember/test-helpers';
+import { render, click, visit, currentURL } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
+import EmberObject from '@ember/object';
+import hbs from 'htmlbars-inline-precompile';
 
 module('Acceptance | list rentals', function(hooks) {
   setupApplicationTest(hooks);
+
+  hooks.beforeEach(function () {
+    this.rental = EmberObject.create({
+      image: 'fake.png',
+      title: 'test-title',
+      owner: 'test-owner',
+      type: 'test-type',
+      city: 'test-city',
+      bedrooms: 3
+    });
+  })
+
+  test('should display rental details', async function(assert) {
+    await render(hbs`{{rental-listing rental=rental}}`);
+    assert.equal(this.$('.listing h3').text(), 'test-title', 'Title: test-title');
+    assert.equal(this.$('.listing .owner').text().trim(), 'Owner: test-owner', 'Owner: test-owner');
+  });
+
+  test('should toggle wide class on click', async function(assert){
+    await render(hbs`{{rental-listing rental=rental}}`);
+    assert.notOK(this.element.querySelector('.image.wide'), 'initially rendered small');
+    await click('.image');
+    assert.ok(this.element.querySelector('.image.wide'), 'rendered wide after click');
+    await click('.image');
+    assert.notOk(this.element.querySelector('.image.wide'), 'rendered small after second click');
+  });
 
   test('should show rentals as the home page', async function (assert) {
     await visit('/');
